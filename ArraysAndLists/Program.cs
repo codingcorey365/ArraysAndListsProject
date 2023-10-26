bf3ca5592ff472d4b4ea4a3d0ca1f48d23f9b343
Corey Schroeder <codingcorey365@gmail.com>
2023-10-25 23:28:16 -0500

    mistakenly finished code did not get pushed to repo

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ArraysAndLists
 {
@@ -7,41 +8,48 @@ namespace ArraysAndLists
     {
         static void Main(string[] args)
         {
-            //TODO:
-
             // Create an int Array and populate numbers 1-10
 
-             int[] numRange10 = new int[10];
-             numRange10[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            Console.WriteLine("Create an int Array and populate numbers 1-10 \n" );            
+            int[] numRange10 = Enumerable.Range(1, 10).ToArray();
+            foreach (int number in numRange10)
+            {
+                Console.WriteLine($"{number}");
+            }           
 
-
-            /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
-             */
-
+            //Create two Lists of type int. name them even and odd.
+            Console.WriteLine("\nCreate two Lists of type int. Name one List \"evens\" Name the other List \"odds\" \n");
             List<int> evens = new List<int>();
             List<int> odds = new List<int>();
 
-            /* Using either a foreach or for loop,
-             * nest an if statement to check to see
-             *  if a number is even or odd.
-             * Then add those numbers to either the evens List
-             * or the odds List
-             */
+            //Check for odd or even. add to correct list.
+            Console.WriteLine("Using either a foreach or for loop, nest an if statement to check to see if a number is even or odd. Then add those numbers to either the evens List or the odds List \n");
+            
             foreach (int number in numRange10)
             {
                 if (number % 2 == 0) 
                 {
                     evens.Add(number);                
                 }
+                else
+                {
+                    odds.Add(number);
+                }
             }
 
-            /* Now using foreach or for loops,
-             * display each List of even and odd numbers
-             *
-             * Try to be creative in your display
-             */
+            //Display new list
+            Console.WriteLine("Now using foreach or for loops, display each List of even and odd numbers. Try to be creative in your display \n");
+            Console.WriteLine($"the numbers in the odds list are as follows\n");
+            foreach (int number in odds)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine($"the numbers in the evens list are as follows\n");
+            foreach (int number in evens)
+            {
+                Console.WriteLine(number);  
+            }
         }
     }
 }
